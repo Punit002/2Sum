04152442fc3de1c0253d79bb7192da217e3a5881
Punit Parihar <punit.parihar@exxat.com>
2023-05-20 08:26:51 +0530

    Add project files.

---
+++ 2Sum_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace _2Sum_2
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new ();
+
+            var result = solution.TwoSum(new int[] { 3, 3 }, 6);
+
+            Console.WriteLine(JsonSerializer.Serialize(result));
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            List<int> result = new();
+            for(int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if(nums[j] + nums[i] == target)
+                    {
+                        result.Add(i);
+                        result.Add(j);
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
